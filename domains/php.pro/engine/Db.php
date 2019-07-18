df1fec794b98f22c0c8cb507fe4263e53e882b25
Efrony <efron.vit@gmail.com>
2019-07-18 12:31:53 +0300

    ...

--- domains/php.pro/engine/Db.php
+++ domains/php.pro/engine/Db.php
@@ -2,15 +2,20 @@
 //=========================test=================
 class Db
 {
+    private $connection = null;
     private $config = [
         'driver' => 'mysql',
         'host' => 'localhost',
         'login' => 'root',
         'password' => '',
         'charset' => 'utf8',
-        'database' => 'shop'
+        'database' => 'shop',
+        'opt' => [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // fetch(PDO::FETCH_ASSOC)) fetch(PDO::FETCH_LAZY))
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ]
     ];
-    private $connection = null;
 
     private function getDSN()
     {
@@ -18,35 +23,57 @@ class Db
         $host = $this->config['host'];
         $db =  $this->config['database'];
         $charset = $this->config['charset'];
-
         $dsn = "$driver:host=$host;dbname=$db;charset=$charset";
         return $dsn;
     }
 
-    public function getConnection()
+    private function getConnection()
     {
         if (is_null($this->connection)) {
             $dsn = $this->getDSN();
             $user = $this->config['login'];
             $pass = $this->config['password'];
-            $opt = [
-                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // fetch(PDO::FETCH_ASSOC)) fetch(PDO::FETCH_LAZY))
-                PDO::ATTR_EMULATE_PREPARES   => false,
-            ];
+            $opt = $this->config['opt'];
+
             $this->connection = new PDO($dsn, $user, $pass, $opt);
         }
         return $this->connection;
     }
+
+    private function query($sql, $params)
+    {
+        $stmt = $this->getConnection()->prepare($sql);
+        $stmt->execute($params);
+        return $stmt;
+    }
+
+    public function execute($sql, $params)
+    {
+        $this->query($sql, $params);
+        return true;
+    }
+
+    public function queryAll($sql, $params = [])
+    {
+        return $this->query($sql, $params)->fetchAll();
+    }
+
+    public function queryOne($sql, $params = [])
+    {
+        return $this->queryAll($sql, $params)[0];
+    }
 }
 
+/*
 $db = new Db;
-$stmt = $db->getConnection()->query('SELECT * FROM product');
+$stmt = $db->queryOne('SELECT * FROM product WHERE id = :id', ['id' => 10]);
+var_dump($stmt);
 
-/*while ($row = $stmt->fetch()) {
+while ($row = $stmt->fetch()) {
     echo $row['name'] . "<br>";
     echo  "<br>";
 }
+
 die;*/
 //==============================================
 
